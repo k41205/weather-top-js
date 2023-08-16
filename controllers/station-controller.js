a07b891c6ccb917fd51a354288ae84ed36dbc44d
k41205 <giacomocavarretta@gmail.com>
2023-08-16 08:51:51 +0100

    added timestamp field in measure controller and view

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -1,22 +1,33 @@
-import { stationStore } from "../models/station-store.js";
-import { measureStore } from "../models/measure-store.js";
+import { stationStore } from '../models/station-store.js';
+import { measureStore } from '../models/measure-store.js';
 
 export const stationController = {
   async index(request, response) {
     const station = await stationStore.getStationById(request.params.id);
-    const lastMeasure = await measureStore.getLastMeasureByStationId(station._id);
+    const lastMeasure = await measureStore.getLastMeasureByStationId(
+      station._id
+    );
 
     const viewData = {
-      name: "Station",
+      name: 'Station',
       station: station,
       lastMeasure: lastMeasure,
     };
-    response.render("station-view", viewData);
+    response.render('station-view', viewData);
   },
 
   async addMeasure(request, response) {
     const station = await stationStore.getStationById(request.params.id);
     const newMeasure = {
+      time: new Date().toLocaleString('en-UK', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+      }),
       code: Number(request.body.code),
       temp: Number(request.body.temp),
       windSpeed: Number(request.body.windSpeed),
@@ -25,6 +36,6 @@ export const stationController = {
     };
     console.log(`adding a new measure`);
     await measureStore.addMeasure(station._id, newMeasure);
-    response.redirect("/station/" + station._id);
+    response.redirect('/station/' + station._id);
   },
 };
